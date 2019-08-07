b181fda3bccaa28a30bc762002d2ae295b8a64e9
Cathy Wong <wong.catherine.mail@gmail.com>
2019-08-07 15:10:36 -0400

    Coalesce stems

--- corpora/cross_corpora.py
+++ corpora/cross_corpora.py
@@ -8,12 +8,20 @@ from collections import defaultdict
 if __name__ == "__main__":
 	corpora = [f for f in os.listdir('.') if f.endswith('.txt')]
 	verbs = defaultdict(lambda: [0] * len(corpora))
+
+	stems = defaultdict(list)
 	for i, corpus in enumerate(corpora):
 		with open(corpus) as f:
 			lines = [l.strip() for l in f.readlines()]
 		for w in lines:
-			verbs[w][i] = 1
+			stem = w.split()[0]
+			if len(w.split()) > 1:
+				stems[stem].append(' '.join(w.split()[1:]))
+			verbs[stem][i] = 1
 
 	print("\t".join(['stem'] + corpora))
-	for w in sorted(list(verbs.keys()), key=lambda k: sum(verbs[k]), reverse=True):
-		print('\t'.join([w] + [str(i) for i in verbs[w]]))
+	for stem in sorted(list(verbs.keys()), key=lambda k: (-sum(verbs[k]), k)):
+		w = stem
+		if stem in stems.keys():
+			w = "%s (%s)" % (stem, ','.join(set(stems[stem])))
+		print('\t'.join([w] + [str(i) for i in verbs[stem]]))
